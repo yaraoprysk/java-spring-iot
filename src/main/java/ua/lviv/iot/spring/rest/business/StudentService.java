1e0c28e42afd0a96ed359d941513879d70a2fb3c
damoklov <mishanya@protonmail.com>
2020-03-27 10:11:02 +0200

    Added two new packages

---
+++ src/main/java/ua/lviv/iot/spring/rest/business/StudentService.java
@@ -0,0 +1,16 @@
+package ua.lviv.iot.spring.rest.business;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ua.lviv.iot.spring.rest.dataaccess.StudentRepository;
+import ua.lviv.iot.spring.rest.model.Student;
+
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public Student createStudent(Student student) {
+        return studentRepository.save(student);
+    }
+}
