88511f5f66de5d1cb1dd5bf68397173d29cd708d
damoklov <mishanya@protonmail.com>
2020-04-03 10:17:35 +0300

    Updated data access modules

--- src/main/java/ua/lviv/iot/spring/rest/dataaccess/GroupRepository.java
+++ src/main/java/ua/lviv/iot/spring/rest/dataaccess/GroupRepository.java
@@ -1,4 +1,8 @@
 package ua.lviv.iot.spring.rest.dataaccess;
 
-public interface OrderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import ua.lviv.iot.spring.rest.model.Group;
+
+public interface GroupRepository extends JpaRepository<Group, Integer> {
+
 }
