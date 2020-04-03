340cce9c375802235f3c133e75ba50fc173ee6e7
damoklov <mishanya@protonmail.com>
2020-04-03 10:18:03 +0300

    Updated business level modules

--- src/main/java/ua/lviv/iot/spring/rest/business/GroupService.java
+++ src/main/java/ua/lviv/iot/spring/rest/business/GroupService.java
@@ -1,9 +1,20 @@
 package ua.lviv.iot.spring.rest.business;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.stereotype.Service;
+import ua.lviv.iot.spring.rest.dataaccess.GroupRepository;
+import ua.lviv.iot.spring.rest.model.Group;
+
+@Service
 public class GroupService {
 
     @Autowired
-    private GroupService groupRepository;
+    private GroupRepository groupRepository;
+
+    public List<Group> findAll() {
+        return this.groupRepository.findAll();
+    }
 }
--- src/main/java/ua/lviv/iot/spring/rest/business/StudentService.java
+++ src/main/java/ua/lviv/iot/spring/rest/business/StudentService.java
@@ -1,9 +1,13 @@
 package ua.lviv.iot.spring.rest.business;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ua.lviv.iot.spring.rest.dataaccess.StudentRepository;
 import ua.lviv.iot.spring.rest.model.Student;
+
+import java.util.List;
 
 @Service
 public class StudentService {
@@ -13,4 +17,40 @@ public class StudentService {
     public Student createStudent(Student student) {
         return studentRepository.save(student);
     }
+    public ResponseEntity<Student> getStudent(Integer studentId) {
+        if(studentRepository.existsById(studentId)) {
+            Student selectedSweater = studentRepository.findById(studentId).get();
+            return new ResponseEntity<Student>(selectedSweater,HttpStatus.OK);
+        } else {
+            return new ResponseEntity<Student>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public ResponseEntity<Student> updateStudent(Student studentToUpdate, Integer id) {
+        if (studentRepository.existsById(id)) {
+            studentToUpdate.setId(id);
+            Student updatedSweater = studentRepository.save(studentToUpdate);
+            return new ResponseEntity<Student>(updatedSweater, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<Student>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public HttpStatus deleteStudent(Integer studentId) {
+        if (studentRepository.existsById(studentId)) {
+            studentRepository.deleteById(studentId);
+            return HttpStatus.OK;
+        } else {
+            return HttpStatus.NOT_FOUND;
+        }
+    }
+
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
 }
