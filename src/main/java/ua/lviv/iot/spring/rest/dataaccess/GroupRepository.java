92900a1e0f23fd55d04b2fd454474fe91911a264
damoklov <mishanya@protonmail.com>
2020-04-06 21:00:42 +0300

    Updated Group repository

--- src/main/java/ua/lviv/iot/spring/rest/dataaccess/GroupRepository.java
+++ src/main/java/ua/lviv/iot/spring/rest/dataaccess/GroupRepository.java
@@ -1,8 +1,10 @@
 package ua.lviv.iot.spring.rest.dataaccess;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ua.lviv.iot.spring.rest.model.Group;
 
+@Repository
 public interface GroupRepository extends JpaRepository<Group, Integer> {
 
 }
